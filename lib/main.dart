212c2d8eeba3805e12ad7b9042f5206efb6ba4ee
Nat3y <khalednatey85@gmail.com>
2024-04-11 17:02:28 +0200

    add cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movies/ui/home.dart';
 import 'package:movies/ui/tabs/firestpage/detailfilmscreen.dart';
-void main() {
+
+import 'firebase_options.dart';
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
